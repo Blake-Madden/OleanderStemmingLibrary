539dd76f7029ade0cce8e59579f764f583a08fed
Patrick Cox <coxpatrick74@gmail.com>
2019-06-27 15:42:45 -0400

    Fix 3 warnings

--- include/pstem/indexing/string_util.h
+++ include/pstem/indexing/string_util.h
@@ -41,11 +41,11 @@ inline wchar_t tolower_western(const wchar_t c)
 // strtol
 inline double strtol(const char * str, char ** strend, int radix)
 {
-    return std::strtol(str, strend, radix);
+    return (double)std::strtol(str, strend, radix);
 }
 inline double strtol(const wchar_t * str, wchar_t ** strend, int radix)
 {
-    return std::wcstol(str, strend, radix);
+    return (double)std::wcstol(str, strend, radix);
 }
 
 // strtod
