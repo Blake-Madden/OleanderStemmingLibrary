3619570a41d8560461219fd28589d704e469f4e5
Blake Madden <madindayton@outlook.com>
2022-11-08 21:57:34 -0500

    Add boilerplate unit test runner (Catch2)

---
+++ tests/englishtests.cpp
@@ -0,0 +1,4 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <catch2/benchmark/catch_benchmark_all.hpp>
+#include "../src/english_stem.h"
