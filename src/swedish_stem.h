e372a249c7f9de0fb5bd4810909828ff5a2bd33a
Blake Madden <madindayton@outlook.com>
2023-01-16 11:31:02 -0500

    Remove old namespace, compile fix

--- src/swedish_stem.h
+++ src/swedish_stem.h
@@ -76,7 +76,7 @@ namespace stemming
             //reset internal data
             stem<string_typeT>::reset_r_values();
 
-            std::transform(text.begin(), text.end(), text.begin(), string_util::full_width_to_narrow);
+            std::transform(text.begin(), text.end(), text.begin(), full_width_to_narrow);
             stem<string_typeT>::trim_western_punctuation(text);
 
             //see where the R1 section begins
