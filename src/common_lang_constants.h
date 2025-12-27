45eb3485f67b94d67bb883601ed65459975b3960
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-12-27 10:06:40 -0500

    Small modernization

--- src/common_lang_constants.h
+++ src/common_lang_constants.h
@@ -185,7 +185,7 @@ namespace common_lang_constants
     const std::wstring COMPOUND_WORD_SEPARATORS{ HYPHEN, HYPHEN_FULL_WIDTH, SOFT_HYPHEN,
                                                  FORWARD_SLASH, FORWARD_SLASH_FULL_WIDTH,
                                                  BACK_SLASH, BACK_SLASH_FULL_WIDTH };
-    inline constexpr wchar_t NUMBERS_AND_DOT[]{
+    const std::wstring NUMBERS_AND_DOT{
         NUMBER_0, NUMBER_1, NUMBER_2, NUMBER_3, NUMBER_4,
         NUMBER_5, NUMBER_6, NUMBER_7, NUMBER_8, NUMBER_9,
         NUMBER_0_FULL_WIDTH, NUMBER_1_FULL_WIDTH,
@@ -193,7 +193,7 @@ namespace common_lang_constants
         NUMBER_4_FULL_WIDTH, NUMBER_5_FULL_WIDTH,
         NUMBER_6_FULL_WIDTH, NUMBER_7_FULL_WIDTH,
         NUMBER_8_FULL_WIDTH, NUMBER_9_FULL_WIDTH,
-        PERIOD, 0 };
+        PERIOD };
 }
 
 /** @}*/
